66e1a8b7de006ba5b34d6ea78c7c4674c90ea152
Rajatpradhan123 <pradhanrajat1999@gmai.com>
2024-07-21 11:10:28 +0530

    Connected to mongo Atlas

---
+++ models/connect.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+
+exports.connect = async () => {
+
+try { 
+    const conn = await mongoose.connect(process.env.MONGO_URI)
+   console.log(`MONGODB ATLAS CONNECTED : ${conn.connection.host}`)
+} catch (error) {
+    console.log(error.message)
+}
+
+
+
+}
+
+
