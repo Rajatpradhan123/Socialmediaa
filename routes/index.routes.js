4f63adf079deefab3766284c5577aabe0f190559
Rajatpradhan123 <pradhanrajat1999@gmai.com>
2024-07-21 18:18:45 +0530

    Register and Login Page Complete.

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/Register', function(req, res, next) {
+  res.render('Register', { title: 'Express' });
+});
+
+router.get('/About', function(req, res, next) {
+  res.render('About', { title: 'Express' });
+});
+
+router.get('/Contacts', function(req, res, next) {
+  res.render('Contacts', { title: 'Express' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Express' });
+});
+
 module.exports = router;
